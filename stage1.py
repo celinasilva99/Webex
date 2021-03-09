37ddb8c952856a44e38875ea146acc73e490436f
celinasilva99 <36280174+celinasilva99@users.noreply.github.com>
2021-03-09 11:08:58 +0000

    Removed confidential info

--- stage1.py
+++ stage1.py
@@ -3,7 +3,7 @@ import json
 from env import config
 
 baseurl = 'https://webexapis.com/v1/rooms'
-token = "NTdhMmZiYWEtZjIyNy00ZjVmLTkyMTYtNzJjMmRiOTM1NmE0MDk2YjY3YjgtNDcx_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
+token = "enter token here"
 
 headers = {'Authorization': f"Bearer {token}",
            'Content-Type': 'application/json'}
@@ -26,7 +26,7 @@ mess_url = 'https://webexapis.com/v1/messages'
 
 mess_body = {
     'roomId': roomId,
-    'text': 'Hi, Welcome to Celinas Space'
+    'text': 'Hi, Welcome'
 }
 
 mess_response = requests.post(
@@ -36,7 +36,7 @@ memberships = 'https://webexapis.com/v1/memberships'
 
 memb_body = {
     'roomId': roomId,
-    'personEmail': 'andrusu@cisco.com',
+    'personEmail': 'user@company.com',
     'isModerator': False
 }
 
